afce4a0107e8ae4d331ecd8d9b6aed30bb73ccb7
dburt667 <burtd667@gmail.com>
2016-10-18 21:35:25 -0400

    Added a Function module framework

---
+++ Public/FunctionFramework.psm1
@@ -0,0 +1,23 @@
+<#
+.Synopsis
+This is a sample framwork for a Module
+
+.Description
+You describe what your function does here
+.Parameter Start
+
+.Parameter End
+
+.Example
+    # Enter your examples here
+
+#>
+Begin Region
+function function name{
+    # Parameter help description
+    [Parameter(AttributeValues)]
+    [ParameterType]
+    $ParameterName
+}
+
+End Region
